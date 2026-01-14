b25f36a93f973e2f451cb93f7cf1ac4f28529cea
eNoLJ <derosatam76@gmail.com>
2026-01-14 09:51:59 +0900

    init commit

---
+++ src/main/java/com/spartabugkiller/ecommercebackofficeproject/EcommerceBackofficeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spartabugkiller.ecommercebackofficeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackofficeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackofficeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spartabugkiller/ecommercebackofficeproject/EcommerceBackofficeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartabugkiller.ecommercebackofficeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackofficeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
