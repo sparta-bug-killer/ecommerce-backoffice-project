2170bb8a089ce9fe31b2bcc5eb60837a375f5f6c
EUNSIK JUNG <bamtol2100@naver.com>
2026-01-14 19:32:53 +0900

    feat: 상품 리스트 조회 API 구현 (필터/페이징/정렬)

--- src/main/java/com/spartabugkiller/ecommercebackofficeproject/product/service/ProductService.java
+++ src/main/java/com/spartabugkiller/ecommercebackofficeproject/product/service/ProductService.java
@@ -1,9 +1,29 @@
 package com.spartabugkiller.ecommercebackofficeproject.product.service;
 
+import com.spartabugkiller.ecommercebackofficeproject.product.dto.response.ProductListResponse;
+import com.spartabugkiller.ecommercebackofficeproject.product.entity.ProductStatus;
+import com.spartabugkiller.ecommercebackofficeproject.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.data.domain.Pageable;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    /**
+     *  상품 리스트 조회
+     */
+    @Transactional(readOnly = true)
+    public Page<ProductListResponse> getProducts (ProductStatus status, Long categoryId, String keyword, Pageable pageable) {
+
+        return productRepository
+                .findProducts(categoryId, status, keyword, pageable)
+                .map(ProductListResponse::new);
+    }
 }
